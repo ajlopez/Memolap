9f4f375f9d237cb85af4047dd038d65aab5c8e93
ajlopez <webmaster@ajlopez.com>
2013-07-04 07:18:22 -0300

    DataBank first test

---
+++ Src/Memolap.Core.Test/DataBankTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Memolap.Core.Test
+{
+    [TestClass]
+    public class DataBankTests
+    {
+        [TestMethod]
+        public void CreateDataBank()
+        {
+            DataBank bank = new DataBank("Test");
+
+            Assert.AreEqual("Test", bank.Name);
+            Assert.IsNotNull(bank.Dimensions);
+            Assert.AreEqual(0, bank.Dimensions.Count);
+        }
+    }
+}
---
+++ Src/Memolap.Core/DataBank.cs
@@ -0,0 +1,22 @@
+﻿namespace Memolap.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class DataBank
+    {
+        private string name;
+        private IList<Dimension> dimensions = new List<Dimension>();
+
+        public DataBank(string name) 
+        {
+            this.name = name;
+        }
+
+        public string Name { get { return this.name; } }
+
+        public IList<Dimension> Dimensions { get { return this.dimensions; } }
+    }
+}
